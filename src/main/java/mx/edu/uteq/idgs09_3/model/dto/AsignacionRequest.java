186cf0dd9620c7fd8020459351ab656555487987
Marco Polo Cruz Ceron <2021371066@uteq.edu.mx>
2025-08-19 13:46:04 -0600

    Update AsignacionRequest.java

--- src/main/java/mx/edu/uteq/idgs09_3/model/dto/AsignacionRequest.java
+++ src/main/java/mx/edu/uteq/idgs09_3/model/dto/AsignacionRequest.java
@@ -1,1 +1,13 @@
+package mx.edu.uteq.idgs09_3.model.dto;
 
+// Clase DTO para la request
+public class AsignacionRequest {
+    private int profesorId;
+    private int categoriaId;
+    
+    // getters y setters
+    public int getProfesorId() { return profesorId; }
+    public void setProfesorId(int profesorId) { this.profesorId = profesorId; }
+    public int getCategoriaId() { return categoriaId; }
+    public void setCategoriaId(int categoriaId) { this.categoriaId = categoriaId; }
+}
